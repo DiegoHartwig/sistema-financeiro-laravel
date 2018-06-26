443bdf396eeb828b408a9028f47089a09438bdab
Diego Hartwig <hartwig.diego@gmail.com>
2018-06-26 20:58:48 -0300

    Instalacao admin lte e criacao do seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'      => 'Diego Hartwig',
+            'email'     => 'hartwig.diego@gmail.com',
+            'password'  => bcrypt('sistema'), 
+        ]);
+    }
+}
