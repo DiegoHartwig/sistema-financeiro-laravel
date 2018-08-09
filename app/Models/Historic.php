73b8f8e50322f7ec03696166aa6f4db5c0fd5b24
Diego Hartwig <hartwig.diego@gmail.com>
2018-08-09 16:11:50 -0300

    Formatação de data com Carbon

--- app/Models/Historic.php
+++ app/Models/Historic.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Historic extends Model
 {
-    protected $fillable = ['type', 'amount', 'total_before', 'total_after', 'user_id_transaction', 'date'];   
+    protected $fillable = ['type', 'amount', 'total_before', 'total_after', 'user_id_transaction', 'date'];  
+    
+    public function getDateAttribute($valor)
+    {
+        return Carbon::parse($valor)->format('d/m/Y');
+    }
 }
